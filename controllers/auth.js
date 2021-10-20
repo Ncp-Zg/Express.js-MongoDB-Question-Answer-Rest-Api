f68a132f56c9e792c4a860dbe62364c37821da1b
Necip <ncptzgn@gmail.com>
2021-10-20 11:15:09 +0300

    Editing user info

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,6 +152,20 @@ const resetPassword = asyncErrorWrapper(async (req, res, next) => {
 
 });
 
+const editDetails = asyncErrorWrapper(async (req, res, next) => {
+  const editInformation = req.body;
+  const user = await User.findByIdAndUpdate(req.user.id,editInformation,{
+    new: true,
+    runValidators: true
+  })
+  return res.status(200).json({
+    success: true,
+    data : user
+
+  })
+  
+});
+
 module.exports = {
   register,
   getUser,
@@ -160,4 +174,5 @@ module.exports = {
   imageUpload,
   forgotPassword,
   resetPassword,
+  editDetails
 };
--- routers/auth.js
+++ routers/auth.js
@@ -1,20 +1,30 @@
 const express = require("express");
-const {register,getUser,login,logout,imageUpload,forgotPassword, resetPassword} = require("../controllers/auth");
+const {
+  register,
+  getUser,
+  login,
+  logout,
+  imageUpload,
+  forgotPassword,
+  resetPassword,
+  editDetails,
+} = require("../controllers/auth");
 const { getAccessToRoute } = require("../middlewares/authorization/auth");
 const profileImageUpload = require("../middlewares/libraries/profileImageUpload");
 
 const router = express.Router();
 
-
-router.post("/register",register)
-router.post("/login",login)
-router.get("/profile",getAccessToRoute, getUser);
-router.get("/logout",getAccessToRoute,logout)
-router.post("/upload",[getAccessToRoute, profileImageUpload.single("profile_image")],imageUpload)
-router.post("/forgotpassword",forgotPassword)
-router.put("/resetpassword",resetPassword)
-
-
-
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", getAccessToRoute, getUser);
+router.get("/logout", getAccessToRoute, logout);
+router.post(
+  "/upload",
+  [getAccessToRoute, profileImageUpload.single("profile_image")],
+  imageUpload
+);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword", resetPassword);
+router.put("/edit", getAccessToRoute, editDetails);
 
 module.exports = router;
