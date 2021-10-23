fa14e20d6797371926c1d4da9820f9ce30a37b85
Necip <ncptzgn@gmail.com>
2021-10-23 12:43:58 +0300

    get all questions

--- controllers/question.js
+++ controllers/question.js
@@ -16,6 +16,21 @@ const askNewQuestion = asyncErrorWrapper(async (req,res,next)=>{
     });
 });
 
+
+const getAllQuestions = asyncErrorWrapper(async (req,res,next)=>{
+    const questions = await Question.find();
+
+
+    return res.status(200).json({
+        success: true,
+        data: questions
+    });
+
+
+
+});
+
 module.exports = {
     askNewQuestion,
+    getAllQuestions
 }
--- routers/question.js
+++ routers/question.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { askNewQuestion } = require("../controllers/question");
+const { askNewQuestion,getAllQuestions } = require("../controllers/question");
 
 const {getAccessToRoute} = require ("../middlewares/authorization/auth")
 
 const router = express.Router();
 
 
+router.get("/", getAllQuestions)
 router.post("/ask", getAccessToRoute, askNewQuestion)
 
 
