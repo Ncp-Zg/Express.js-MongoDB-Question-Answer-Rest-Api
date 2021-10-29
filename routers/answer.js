952e268e9d68581e89dc705b07120a416820b3bf
Necip <ncptzgn@gmail.com>
2021-10-29 14:44:27 +0300

    delete,like and undo operations for answers

--- routers/answer.js
+++ routers/answer.js
@@ -9,6 +9,9 @@ const {
   getAllAnswersByQuestion,
   getSingleAnswer,
   editAnswer,
+  deleteAnswer,
+  likeAnswer,
+  undoLikeAnswer
 } = require("../controllers/answer");
 const {
   checkQuestionAndAnswerexist,
@@ -17,10 +20,25 @@ const {
 router.post("/", getAccessToRoute, addNewAnswerToQuestion);
 router.get("/", getAllAnswersByQuestion);
 router.get("/:answer_id", checkQuestionAndAnswerexist, getSingleAnswer);
+router.get(
+  "/:answer_id/like",
+  [checkQuestionAndAnswerexist, getAccessToRoute],
+  likeAnswer
+);
+router.get(
+  "/:answer_id/undo_like",
+  [checkQuestionAndAnswerexist, getAccessToRoute],
+  undoLikeAnswer
+);
 router.put(
   "/:answer_id/edit",
   [checkQuestionAndAnswerexist, getAccessToRoute, getAnswerOwnerAccess],
   editAnswer
+);
+router.delete(
+  "/:answer_id/delete",
+  [checkQuestionAndAnswerexist, getAccessToRoute, getAnswerOwnerAccess],
+  deleteAnswer
 );
 
 module.exports = router;
