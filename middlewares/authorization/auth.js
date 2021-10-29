d398890d1a38ed0c42719240e366f16270a21fd6
Necip <ncptzgn@gmail.com>
2021-10-29 13:50:24 +0300

    edit answer

--- middlewares/authorization/auth.js
+++ middlewares/authorization/auth.js
@@ -1,64 +1,76 @@
 const CustomError = require("../../helpers/error/CustomError");
 const jwt = require("jsonwebtoken");
-const asyncErrorWrapper = require("express-async-handler")
-const { isTokenIncluded,getAccessTokenFromHeader } = require("../../helpers/authorization/tokenHelpers");
+const asyncErrorWrapper = require("express-async-handler");
+const {
+  isTokenIncluded,
+  getAccessTokenFromHeader,
+} = require("../../helpers/authorization/tokenHelpers");
 const User = require("../../models/User");
 const Question = require("../../models/Question");
+const Answer = require("../../models/Answer");
 
-const getAccessToRoute = (req,res,next)=>{
-    //Token
+const getAccessToRoute = (req, res, next) => {
+  //Token
 
-    const {JWT_SECRET_KEY} = process.env
-    if(!isTokenIncluded(req)){
-        return next(new CustomError("You are not authorized to access this route",401))
+  const { JWT_SECRET_KEY } = process.env;
+  if (!isTokenIncluded(req)) {
+    return next(
+      new CustomError("You are not authorized to access this route", 401)
+    );
+  }
+  const accessToken = getAccessTokenFromHeader(req);
+  jwt.verify(accessToken, JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return next(
+        new CustomError("You are not authorized to access this route", 401)
+      );
     }
-    const accessToken=getAccessTokenFromHeader(req);
-    jwt.verify(accessToken,JWT_SECRET_KEY,(err,decoded)=>{
-        if (err) {
-            return next(
-            new CustomError("You are not authorized to access this route",401)
-          );
-        }
-        req.user ={
-            id: decoded.id,
-            name: decoded.name
-        }
-        
-        next();
-    })
-    
+    req.user = {
+      id: decoded.id,
+      name: decoded.name,
+    };
 
-    //CustomError
+    next();
+  });
+
+  //CustomError
 };
 
-const getAdminAccess = asyncErrorWrapper(async (req,res,next) => {
+const getAdminAccess = asyncErrorWrapper(async (req, res, next) => {
+  const { id } = req.user;
 
-    const {id} = req.user;
+  const user = await User.findById(id);
 
-    const user = await User.findById(id);
-    
-    if(user.role !== "admin" ){
-        return next(new CustomError("Only admins can access this router",403))
-    }
-    next();
+  if (user.role !== "admin") {
+    return next(new CustomError("Only admins can access this router", 403));
+  }
+  next();
+});
 
-})
+const getQuestionOwnerAccess = asyncErrorWrapper(async (req, res, next) => {
+  const userId = req.user.id;
+  const questionId = req.params.id;
+  const question = await Question.findById(questionId);
 
-const getQuestionOwnerAccess = asyncErrorWrapper(async (req,res,next) => {
+  if (question.user != userId) {
+    return next(new CustomError("Only owner can handle this operation", 403));
+  }
+  next();
+});
 
-    const userId = req.user.id;
-    const questionId = req.params.id;
-    const question = await Question.findById(questionId);
+const getAnswerOwnerAccess = asyncErrorWrapper(async (req, res, next) => {
+  const userId = req.user.id;
+  const answerId = req.params.answer_id;
+  const answer = await Answer.findById(answerId);
 
-    if(question.user != userId){
-        return next(new CustomError("Only owner can handle this operation",403))
-    }
-    next();
-
-
-})
-module.exports={
-    getAccessToRoute,
-    getAdminAccess,
-    getQuestionOwnerAccess,
-}
+  if (answer.user != userId) {
+    return next(new CustomError("Only owner can handle this operation", 403));
+  }
+  next();
+});
+module.exports = {
+  getAccessToRoute,
+  getAdminAccess,
+  getQuestionOwnerAccess,
+  getAnswerOwnerAccess,
+};
--- routers/answer.js
+++ routers/answer.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const { getAccessToRoute } = require("../middlewares/authorization/auth");
-const router =express.Router({mergeParams:true});
-const {addNewAnswerToQuestion,getAllAnswersByQuestion,getSingleAnswer}=require("../controllers/answer");
-const { checkQuestionAndAnswerexist } = require("../middlewares/database/databaseErrorHelpers");
+const {
+  getAccessToRoute,
+  getAnswerOwnerAccess,
+} = require("../middlewares/authorization/auth");
+const router = express.Router({ mergeParams: true });
+const {
+  addNewAnswerToQuestion,
+  getAllAnswersByQuestion,
+  getSingleAnswer,
+  editAnswer,
+} = require("../controllers/answer");
+const {
+  checkQuestionAndAnswerexist,
+} = require("../middlewares/database/databaseErrorHelpers");
 
-router.post("/",getAccessToRoute,addNewAnswerToQuestion);
-router.get("/", getAllAnswersByQuestion)
-router.get("/:answer_id",checkQuestionAndAnswerexist,getSingleAnswer)
-
+router.post("/", getAccessToRoute, addNewAnswerToQuestion);
+router.get("/", getAllAnswersByQuestion);
+router.get("/:answer_id", checkQuestionAndAnswerexist, getSingleAnswer);
+router.put(
+  "/:answer_id/edit",
+  [checkQuestionAndAnswerexist, getAccessToRoute, getAnswerOwnerAccess],
+  editAnswer
+);
 
 module.exports = router;
