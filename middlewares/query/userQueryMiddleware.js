ce2b16366e25746122fc9b9dcb0da2e78eb858f2
Necip <ncptzgn@gmail.com>
2021-11-11 17:58:58 +0300

    building centered user query middleware

---
+++ middlewares/query/userQueryMiddleware.js
@@ -0,0 +1,29 @@
+const asyncErrorWrapper = require("express-async-handler");
+const { searchHelper, paginationHelper } = require("./queryMiddlewareHelper");
+
+const userQueryMiddleware = function (model, options) {
+  return asyncErrorWrapper(async function (req, res, next) {
+    let query = model.find();
+
+    query = searchHelper("name", query, req);
+
+    const paginationResult = await paginationHelper(model, query, req);
+
+    query = paginationResult.query;
+    pagination = paginationResult.pagination;
+
+    const queryResult = await query.find();
+    
+    res.queryResult ={
+        success: true,
+        count:queryResult.length,
+        pagination:pagination,
+        data:queryResult
+
+    } 
+
+    next();
+  });
+};
+
+module.exports = userQueryMiddleware;
--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { getSingleUser,getAllUsers } = require("../controllers/user.js");
 const { checkUserexist } = require("../middlewares/database/databaseErrorHelpers.js");
+const userQueryMiddleware = require("../middlewares/query/userQueryMiddleware");
+const User = require("../models/User.js");
 const router = express.Router();
 
 
 
-router.get("/",getAllUsers)
+router.get("/",userQueryMiddleware(User),getAllUsers)
 router.get("/:id",checkUserexist, getSingleUser)
 
 
